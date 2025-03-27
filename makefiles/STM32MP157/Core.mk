c52b574ecf11166cc14a48b54dced4282b004484
jamelofs <james.elofson@du.edu>
2025-03-27 14:02:59 -0600

    Added linkscript, and make files for stm32mp1

---
+++ makefiles/STM32MP157/Core.mk
@@ -0,0 +1,19 @@
+CORE_DIR = $(CORESTM_DIR)
+
+
+#Core
+CORE_SRC_DIRS  = src src/STM32 src/STM32/usb2 src/STM32/MCP2517FD
+CORE_SRC_DIRS += src/STM32/system src/STM32/system/STM32MP1xx
+CORE_SRC_DIRS += src/STM32/system/Drivers/CMSIS/Device/ST/STM32MP1xx/Include src/STM32/system/Drivers/CMSIS/Device/ST/STM32MP1xx/Source
+CORE_SRC_DIRS += src/STM32/system/Drivers/STM32MP1xx_HAL_Driver/Inc src/STM32/system/Drivers/STM32MP1xx_HAL_Driver/Src
+CORE_SRC_DIRS += src/STM32/system/CMSIS/CMSIS/Core/Include src/STM32/system/Drivers/CMSIS/Device/ST/STM32MP1xx/Source/Templates/gcc
+CORE_SRC_DIRS += src/STM32/variants/STM32MP157 src/STM32/ConfigurableUART src/STM32/SharedSPI src/STM32/PWM src/STM32/SDIO src/STM32/CRC32
+
+CORE_SRC = $(CORE_DIR) $(addprefix $(CORE_DIR)/, $(CORE_SRC_DIRS))
+CORE_INCLUDES = $(addprefix -I, $(CORE_SRC))
+
+#Find all c and c++ files for Core
+CORE_OBJ_SRC_C    += $(foreach src, $(CORE_SRC), $(wildcard $(src)/*.c))
+CORE_OBJ_SRC_CXX   += $(foreach src, $(CORE_SRC), $(wildcard $(src)/*.cpp))
+CORE_OBJS = $(patsubst %.c,$(BUILD_DIR)/%.o,$(CORE_OBJ_SRC_C)) $(patsubst %.cpp,$(BUILD_DIR)/%.o,$(CORE_OBJ_SRC_CXX))
+CORE_OBJS += $(BUILD_DIR)/CoreN2G/src/STM32/startup_stm32yyxx.o
